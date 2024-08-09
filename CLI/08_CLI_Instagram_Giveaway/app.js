1356f9ed79b246575b0f45afa64b3edb79850862
gmoinbong <vladosos.scherba@gmail.com>
2024-08-09 16:07:16 +0300

    logic updated

--- CLI/08_CLI_Instagram_Giveaway/app.js
+++ CLI/08_CLI_Instagram_Giveaway/app.js
@@ -1,19 +1,9 @@
 const fs = require('fs').promises;
 const path = require('path');
 const folderPath = './2kk_words_400x400';
+
 const dataSplitter = (filesData) => {
-    return filesData.split(/\s+/).filter(Boolean)
-}
-
-
-function count(userNames, accumulator) {
-    for (let i = 0; i < userNames.length; ++i) {
-        if (!accumulator.hasOwnProperty(userNames[i])) {
-            accumulator[userNames[i]] = 1;
-        } else {
-            ++accumulator[userNames[i]];
-        }
-    }
+    return filesData.toLowerCase().split(/\s+/).filter(Boolean);
 }
 
 async function processFiles(folderPath, callback) {
@@ -22,53 +12,57 @@ async function processFiles(folderPath, callback) {
         for (const fileName of files) {
             const filePath = path.join(folderPath, fileName);
             const fileContent = await fs.readFile(filePath, 'utf8');
-            callback(fileContent, fileName)
+            callback(fileContent, fileName);
         }
     } catch (error) {
         console.error('Error reading files:', error);
     }
-
-}
-
-function uniqueUserNamesCount(filesData) {
-    const userNamesCount = {};
-    const userNames = dataSplitter(filesData)
-    count(userNames, userNamesCount)
-
-    const uniqueUserNames = Object.keys(userNamesCount).filter(userName => userNamesCount[userName] === 1);
-    console.log(`Count unique user names: ${uniqueUserNames.length}`);
 }
 
 function uniqueUserNamesCountForOneTime(filesData) {
-    const userNames = dataSplitter(filesData)
+    const userNames = dataSplitter(filesData);
     const uniqueUserNames = new Set(userNames);
 
     console.log(`Number of unique user names existing in all specified files: ${uniqueUserNames.size}`);
 }
 
+async function countUniqueUserNames() {
+    const globalUserNamesCount = {};
+
+    await processFiles(folderPath, (fileContent) => {
+        const userNames = dataSplitter(fileContent);
+        const localUserNamesSet = new Set(userNames);
+
+        localUserNamesSet.forEach(userName => {
+            if (!globalUserNamesCount[userName]) {
+                globalUserNamesCount[userName] = 0;
+            }
+            globalUserNamesCount[userName]++;
+        });
+    });
+
+
+    const uniqueUserNames = Object.keys(globalUserNamesCount).filter(userName => globalUserNamesCount[userName] === 1);
+    console.log(`Count unique user names: ${uniqueUserNames.length}`);
+}
+
 async function countUserNamesInAtLeast10Files() {
-    const userNamesCount = {};
     const fileCount = {};
+
     await processFiles(folderPath, (fileContent, fileName) => {
         const userNames = new Set(dataSplitter(fileContent));
-        for (const userName of userNames) {
+        userNames.forEach(userName => {
             if (!fileCount[userName]) {
                 fileCount[userName] = new Set();
             }
             fileCount[userName].add(fileName);
-        }
+        });
     });
-    for (let userName in fileCount) {
-        userNamesCount[userName] = fileCount[userName].size;
-    }
 
-    const userNamesInAtLeast10Files = Object.keys(userNamesCount).filter(userName => userNamesCount[userName] >= 10)
+
+    const userNamesInAtLeast10Files = Object.keys(fileCount).filter(userName => fileCount[userName].size >= 10);
     console.log(`Number of unique user names existing in at least 10 files: ${userNamesInAtLeast10Files.length}`);
 }
-
-
-
-
 
 const main = async () => {
     const start = performance.now();
@@ -76,16 +70,15 @@ const main = async () => {
     let allContentFromFiles = "";
     await processFiles(folderPath, fileContent => {
         allContentFromFiles += fileContent + " ";
-    })
+    });
 
-    uniqueUserNamesCountForOneTime(allContentFromFiles)
-    uniqueUserNamesCount(allContentFromFiles)
-    await countUserNamesInAtLeast10Files()
+    uniqueUserNamesCountForOneTime(allContentFromFiles); 
+    await countUniqueUserNames(); 
+    await countUserNamesInAtLeast10Files(); 
 
     const end = performance.now();
     const time = (end - start) / 1000;
     console.log("Execution time:", time, "seconds");
-
 }
 
 main();
