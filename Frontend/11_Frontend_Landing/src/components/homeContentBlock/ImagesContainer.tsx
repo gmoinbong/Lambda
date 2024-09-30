b07deca81af177213e4f0bca9cb2d94d91f98f30
gmoinbong <vladosos.scherba@gmail.com>
2024-09-30 13:04:44 +0300

    vercel

---
+++ Frontend/11_Frontend_Landing/src/components/homeContentBlock/ImagesContainer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from './ImagesContainer.module.css';
+
+export const ImagesContainer = () => {
+    return (
+        <div className={styles.imageContainer}>
+            <div className={styles.overlayDiamond}>
+                <div className={styles.redDiamond}>
+                    <div>
+                        <img className={styles.backgroundImageMobile} src={'/assets/background/background-image.png'} alt="application example" />
+                        <img className={styles.backgroundImageDesktop} src={'/assets/background/background-image1.png'} alt="application example" />
+                    </div>
+                </div>
+                <img className={styles.rectangle2} src={'/assets/background/rectangleGroup.png'} alt="" />
+                <img className={styles.rectangle} src={'/assets/background/rectangleGroup.png'} alt="" />
+            </div>
+        </div>
+    );
+};
